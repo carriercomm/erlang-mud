5bb53fb1c9b93c2c6f654a47f0a854e852f7dec0
Stephen Ayotte <stephen.ayotte@gmail.com>
2013-12-18 10:23:16 -0500

    Test coverage for text_hci_mod_lobby.erl

--- src/text_hci_mod_lobby_tests.erl
+++ src/text_hci_mod_lobby_tests.erl
@@ -2,39 +2,41 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("text_hci.hrl").
 
-enter_test() ->
-    code:add_path("meck/ebin"),
-    meck:new(gen_tcp, [unstick]),
-    meck:expect(gen_tcp, send, fun(_Socket, Msg) -> self() ! {tcp_sent, Msg}, ok end),
-    ?assertEqual(#text_hci_data{mod_data = top}, text_hci_mod_lobby:enter(#text_hci_data{})),
-               
-    receive
-        {tcp_sent, Msg} ->
-            ?assertEqual("--You are in the lobby--\n" ++
-                           "\n" ++
-                           "Select an option:\n" ++
-                           "  1) Create a character\n" ++
-                           "  2) Take control of an existing character\n" ++
-                           "  3) Logout\n",
-                         Msg)
-    end,
-    meck:unload(gen_tcp).
+-define(tcptest(F), {setup, fun setup/0, fun cleanup/1, F}).
+
+%-export([setup/0, cleanup/1, handle_input_test_/0]).
+
+enter_top_test_() ->
+    {"Happy path: entering state 'top' prints top level menu and returns expected output",
+     ?tcptest(
+       fun(_) ->
+          EnterResponse = text_hci_mod_lobby:enter(#text_hci_data{}),
+          receive {tcp_sent, Msg} -> ok end,
+          [ ?_assertEqual(#text_hci_data{mod_data = top}, EnterResponse),
+            ?_assertEqual("--You are in the lobby--\n" ++
+                         "\n" ++
+                         "Select an option:\n" ++
+                         "  1) Create a character\n" ++
+                         "  2) Take control of an existing character\n" ++
+                         "  3) Logout\n",
+                       Msg)
+          ]
+        end
+      )
+    }.
 
 handle_input_test_() ->
-    {foreach,
-        fun setup/0,
-        fun cleanup/1,
-        {inorder,
-          [ fun top_input_1/1,
-            fun top_input_2/1,
-            fun top_input_3/1,
-        %    fun top_input_newline_only/1,
-        %    fun top_input_empty/1,
-            fun create_input/1,
-            fun attach_input/1
-          ]
-        }
+    {inorder,
+      [ {"Happy path: input of '1' in state 'top'", ?tcptest(fun top_input_1/1)},
+        {"Happy path: input of '2' in state 'top'", ?tcptest(fun top_input_2/1)},
+        {"Happy path: input of '3' in state 'top'", ?tcptest(fun top_input_3/1)},
+        {"Happy path: input of '\\r\\n' in state 'top'", ?tcptest(fun top_input_newline_only/1)},
+        {"Unhappy path: input of '' in state 'top'", ?tcptest(fun top_input_empty/1)},
+        {"Happy path: input of '\\r\\n' in state 'create'", ?tcptest(fun create_input/1)},
+        {"Happy path: input of '\\r\\n' in state 'attach'", ?tcptest(fun attach_input/1)}
+      ]
     }.
+
 top_input_1(_) ->
     Response1 = text_hci_mod_lobby:handle_input("1\r\n", #text_hci_data{mod_data = top}),
     receive {tcp_sent, Msg1} -> ok end,
@@ -59,17 +61,21 @@ top_input_3(_) ->
 
 top_input_newline_only(_) ->
     NewlineStringResponse = text_hci_mod_lobby:handle_input("\r\n", #text_hci_data{mod_data = top}),
-    % We could catch the "Unrecognized input..." string here, but meh
-    [?_assertEqual({ok, #text_hci_data{mod_data = top}}, NewlineStringResponse)].
+    receive {tcp_sent, NewlineStringMsg} -> ok end,
+    [ ?_assertEqual({ok, #text_hci_data{mod_data = top}}, NewlineStringResponse),
+      ?_assertEqual(io_lib:format("Unrecognized input '~p', try again.\n", ["\r\n"]), NewlineStringMsg) ].
 
 top_input_empty(_) -> 
     EmptyStringResponse = text_hci_mod_lobby:handle_input("", #text_hci_data{mod_data = top}),
-    % We could catch the "Unrecognized input..." string here, but meh
-    [?_assertEqual({ok, #text_hci_data{mod_data = top}}, EmptyStringResponse)].
+    receive {tcp_sent, EmptyStringMsg} -> ok end,
+    [ ?_assertEqual({ok, #text_hci_data{mod_data = top}}, EmptyStringResponse),
+      ?_assertEqual(io_lib:format("Unrecognized input '~p', try again.\n", [""]), EmptyStringMsg) ].
 
 create_input(_) ->
     CreateInputResponse = text_hci_mod_lobby:handle_input("\r\n", #text_hci_data{mod_data = create}),
     receive {tcp_sent, CreateInputMsg} -> ok end,
+    % enter() gets called again here, temporarily, so there's actually a second tcp message to receive
+    % ... but that's temporary, and we're already testing it in top_input_1/1, so I'm leaving it out
     [ ?_assertEqual({ok, #text_hci_data{mod_data = top}}, CreateInputResponse),
       ?_assertEqual("That's nice, dear! Let's go back to the lobby menu now.\n\n", CreateInputMsg)].
 
@@ -84,6 +90,7 @@ attach_input(_) ->
 setup() ->
     code:add_path("meck/ebin"),
     meck:new(gen_tcp, [unstick]),
+    %meck:expect(gen_tcp, send, fun(_Socket, Msg) -> io:format("meck: passing along this msg: ~p~n", [Msg]), self() ! {tcp_sent, Msg}, ok end),
     meck:expect(gen_tcp, send, fun(_Socket, Msg) -> self() ! {tcp_sent, Msg}, ok end),
     meck:expect(gen_tcp, close, fun(_Socket) -> ok end),
     ok.
@@ -92,6 +99,5 @@ cleanup(_Ignored) ->
     receive _Any ->
         cleanup(ok)
     after 0 ->
-        true
-    end,
-    meck:unload(gen_tcp).   
+        meck:unload(gen_tcp)
+    end.
